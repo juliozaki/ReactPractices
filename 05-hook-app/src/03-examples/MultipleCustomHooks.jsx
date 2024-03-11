8090d614908155c7f117feffc56a3bb16a5187c9
JULIO FERNANDO FUENTES PEREZ <Julio.Fernando.Fuentes@ibm.com>
2024-03-11 12:51:12 -0600

    03

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,51 @@
 import { useFetch } from '../hooks/useFetch';
+import { useCounter } from '../hooks/useCounter';
+import { LoadingMessage } from './LoadingMessage';
+import { PokemonCard } from './PokemonCard';
 
 export const MultipleCustomHooks = () => {
 
-    useFetch();
+    const { counter, increment, decrement } = useCounter(1);
+    const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`);
 
     return (
-        <div>MultipleCustomHooks</div>
+        <>
+            <h1>Información de Pokémon</h1>
+            <hr />
+
+            {
+                isLoading
+                    ? <LoadingMessage />
+                    : (
+                        <PokemonCard
+                            id={counter}
+                            name={data.name}
+                            sprites={[
+                                data.sprites.front_default,
+                                data.sprites.front_shiny,
+                                data.sprites.back_default,
+                                data.sprites.back_shiny
+                            ]}
+                        />
+                    )
+            }
+
+            <hr />
+
+            {/*<h2>{data?.name}</h2>*/}
+
+            <button
+                className='btn btn-primary mt-2'
+                onClick={() => counter > 1 ? decrement() : null}
+            >
+                Anterior
+            </button>
+            <button
+                className='btn btn-primary mt-2'
+                onClick={increment}
+            >
+                Siguiente
+            </button>
+        </>
     )
 }
--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -2,25 +2,75 @@
 
 import { useEffect, useState } from "react";
 
-export const useFetch = () => {
+const localCache = {};
+
+export const useFetch = (url) => {
 
     const [state, setState] = useState({
         data: null,
         isLoading: true,
         hasError: false,
-        errorMessaje: null
+        error: null
     });
 
     useEffect(() => {
         getFetch();
     },
-        []
+        [url]
     );
 
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null
+        });
+    }
+
     const getFetch = async () => {
-        const resp = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+
+        if (localCache[url]) {
+            console.log('Usando Caché');
+            setState({
+                data: localCache[url],
+                isLoading: false,
+                hasError: false,
+                error: null
+            });
+
+            return;
+        }
+
+        setLoadingState();
+
+        const resp = await fetch(url);
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        if (!resp.ok) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText
+                }
+            })
+
+            return;
+        }
+
         const data = await resp.json();
-        console.log(data);
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null
+        })
+
+        localCache[url] = data;
     }
 
     return {
