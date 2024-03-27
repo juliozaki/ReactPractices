1e568e19acef74a73f680b0894e59231ea724b96
JULIO FERNANDO FUENTES PEREZ <Julio.Fernando.Fuentes@ibm.com>
2024-03-26 18:24:08 -0600

    06 estructuras y primera parte del proyecto

---
+++ 06-HeroesApp/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from "../../ui/components/NavBar";
+import { MarvelPage } from '../pages/MarvelPage';
+import { DcPage } from '../pages/DcPage';
+import { SearchPage } from '../pages/SearchPage';
+import { HeroPage } from '../pages/HeroPage';
+
+
+export const HeroesRoutes = () => {
+    return (
+        <>
+            <Navbar />
+
+            <div className='container'>
+                <Routes>
+                    <Route path='marvel' element={<MarvelPage />}></Route>
+                    <Route path='dc' element={<DcPage />}></Route>
+                    <Route path='search' element={<SearchPage />}></Route>
+                    <Route path='hero' element={<HeroPage />}></Route>
+
+                    <Route path='/' element={<Navigate to='/marvel' />}></Route>
+                </Routes>
+            </div>
+        </>
+    );
+}
