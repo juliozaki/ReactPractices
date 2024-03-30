e7dd3e2f965631bebaec9a811392fa494f2835fb
JULIO FERNANDO FUENTES PEREZ <Julio.Fernando.Fuentes@ibm.com>
2024-03-30 11:33:20 -0600

    06 se concluye diseño

--- 06-HeroesApp/src/heroes/components/HeroList.jsx
+++ 06-HeroesApp/src/heroes/components/HeroList.jsx
@@ -5,7 +5,10 @@ import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+    const heroes = useMemo(() =>
+        getHeroesByPublisher(publisher),
+        [publisher]
+    );
 
     return (
         <div className='row rows-cols-1 row-cols-md-3 g-3'>
