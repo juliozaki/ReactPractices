0a32025fb2efd036f9860e64c9727eb21f677af7
JULIO FERNANDO FUENTES PEREZ <Julio.Fernando.Fuentes@ibm.com>
2024-04-11 16:45:06 -0600

    07

--- 07-redux-toolkit-rtk/src/main.jsx
+++ 07-redux-toolkit-rtk/src/main.jsx
@@ -10,10 +10,10 @@ import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={ store }>
+    <Provider store={store}>
       {/* <App /> */}
-      {/* <PokemonApp /> */}
-      <TodoApp />
+      <PokemonApp />
+      {/*<TodoApp />*/}
     </Provider>
   </React.StrictMode>
 )
